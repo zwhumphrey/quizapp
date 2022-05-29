e40c538dc1e59216a5f34e410e2dc4b93a9f0257
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-05-29 10:04:39 -0500

    fixed questions in JS

--- assets/script.js
+++ assets/script.js
@@ -9,37 +9,37 @@ var avaliableQuestions = []
 
 var questions = [
     {
-        question: "Commonly used data types DO NOT include:";
-        choice1: "Strings";
-        choice2: "Booleans";
-        choice3: "alerts";
-        choice4: "numbers";
-        anwser: 3;
-    };    
+        question: "Commonly used data types DO NOT include:",
+        choice1: "Strings",
+        choice2: "Booleans",
+        choice3: "alerts",
+        choice4: "numbers",
+        anwser: 3,
+    },
     {
-        question: "The condition in an if/else statement is enclosed within ______.";
-        choice1: "quotes";
-        choice2: "curly brackets";
-        choice3: "parentheses";
-        choice4: "square brackets";
-        answer: 3;
-    };   
+        question: "The condition in an if/else statement is enclosed within ______.",
+        choice1: "quotes",
+        choice2: "curly brackets",
+        choice3: "parentheses",
+        choice4: "square brackets",
+        answer: 3,
+    },
     {
-        question: "Arrays in JavaScript can be used to store ______.";
-        choice1: "numbers and strings";
-        choice2: "other arrays";
-        choice3: "booleans";
-        choice4: "all of the above";
-        answer: 4;
-    };    
+        question: "Arrays in JavaScript can be used to store ______.",
+        choice1: "numbers and strings",
+        choice2: "other arrays",
+        choice3: "booleans",
+        choice4: "all of the above",
+        answer: 4,
+    },
     {
-        question: "String values must be enclosed within ______ when being assigned to variables.";
-        choice1: "commmas";
-        choice2: "curley brackets";
-        choice3: "quotes";
-        choice4: "parentheses";
-        answer: 3;
-    };
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        choice1: "commmas",
+        choice2: "curley brackets",
+        choice3: "quotes",
+        choice4: "parentheses",
+        answer: 3,
+    },
 ];
 
 
